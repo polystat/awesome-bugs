d9d981a930af4210c3a46a7605e349c64d4be979
mximp <mxim.petrov@gmail.com>
2022-10-26 08:23:57 +0300

    33 add glob filer; customized run script

--- scripts/src/utilities.py
+++ scripts/src/utilities.py
@@ -1,5 +1,5 @@
 import os
-
+import glob
 
 def filter_defect(f):
     temp_path = os.path.join("tests", "errors.txt")
@@ -8,11 +8,16 @@ def filter_defect(f):
     return any(e in f for e in errors)
 
 
-# Searching for YAML files in the given path
-def get_test_files_paths(code_path):
+def get_test_files_paths(code_path, glob_pattern=''):
+    """ Searching for YAML files in the given path.
+
+    :param code_path: root path for tests
+    :param glob_pattern: glob to filter files
+    :return: absolute paths to found files
+    """
     test_case_paths = []
-    for root, dirs, files in os.walk(code_path):
-        for file in files:
-            if filter_defect(file) and file.endswith(".yml"):
-                test_case_paths.append(os.path.join(root, file))
+    pattern = glob_pattern if not glob_pattern == '' else '**/*.yml'
+    for file in glob.glob(pattern, root_dir=code_path, recursive=True):
+        if filter_defect(file) and file.endswith(".yml"):
+            test_case_paths.append(os.path.join(code_path, file))
     return test_case_paths
